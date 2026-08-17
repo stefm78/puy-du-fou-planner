5653f8cb6df52473710453d6056cf6e7dcbc3e4f
Stef <104558808+stefm78@users.noreply.github.com>
2026-08-17 22:09:34 +0200

    Bump planner boot version to 1.3.4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (() => {
 'use strict';
-const VERSION='1.3.3';
+const VERSION='1.3.4';
 const STORE='puyPlannerV4';
 const SCRIPT_TIMEOUT_MS=4500;
 const status=document.getElementById('sourceLine');
